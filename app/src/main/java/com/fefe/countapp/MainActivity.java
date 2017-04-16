7014b60c0c05936f77f1fbe958d8f385d724b303
Sakata Yu <imanity.fefe@gmail.com>
2017-04-16 14:53:53 +0900

    かうんとあぷりばーじょん１！

---
+++ app/src/main/java/com/fefe/countapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.fefe.countapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.textView);
+    }
+
+    public void plus(View v) {
+        count++;
+        textView.setText("" + count);
+    }
+}
